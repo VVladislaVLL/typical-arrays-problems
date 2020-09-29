811370dcf32dc0da538632b41d114b1b9c84d162
VladKoch_ <vlad2002kochurko@gmail.com>
2020-09-29 14:57:10 +0300

    feat: implement the solution of task *typical-arrays-problems*

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) return 0;
+  boubleSort(array);
+  return (array.length === 0) ? 0 : array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) return 0;
+  boubleSort(array);
+  return (array.length === 0) ? 0 : array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  boubleSort(array);
+  return (array.length === 0) ? 0 : getSum(array) / array.length;
+}
+
+
+function boubleSort(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        for (let j = i; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                let term = array[i];
+                array[i] = array[j];
+                array[j] = term;
+            }
+        }
+    }
+    return array;
+}
+
+function getSum(array) {
+    let sum = 0;
+    array.forEach(element => sum += element);
+    return sum;
 }
